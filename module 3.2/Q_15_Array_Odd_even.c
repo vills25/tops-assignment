dda1ca267c8bacd42c4dc4baba6bf9aaaa1cb53a
vills25 <vishalsohaliya25@gmail.com>
2023-06-21 11:12:32 +0530

    Corraction

--- module 3.2/Q_15_Array_Odd_even.c
+++ module 3.2/Q_15_Array_Odd_even.c
@@ -1,28 +1,33 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 int main()
 {
-    int arr[10], evenCount=0, oddCount=0,i, odd_sum, evem_sum,sum;
+    int arr[10], evenCount = 0, oddCount = 0, i, odd_sum = 0, evem_sum = 0, sum;
 
     printf("Enter any 10 elements: ");
 
-    for(i=0; i<10; i++)
+    for (i = 0; i < 10; i++)
         scanf("%d", &arr[i]);
-    for(i=0; i<10; i++)
+    for (i = 0; i < 10; i++)
     {
-        if(arr[i]%2==0)
+        if (arr[i] % 2 == 0)
+        {
             evenCount++;
+            evem_sum = evem_sum + arr[i];
+        }
         else
+        {
             oddCount++;
+            odd_sum = odd_sum + arr[i];
+        }
 
-        sum = oddCount + evenCount , &odd_sum,&evem_sum;    
+        sum = oddCount + evenCount;
     }
     printf("\nTotal Even numbers = %d", evenCount);
     printf("\nTotal Odd numbers = %d", oddCount);
-    printf("\nThe Sum of EVEN numbers and Odd numbers is: %d",sum);
+    printf("\nTotal of even sum = %d",evem_sum );
+    printf("\nTotal of odd numbers= %d", odd_sum);
     getch();
 
     return 0;
-
-
-}    
+}
