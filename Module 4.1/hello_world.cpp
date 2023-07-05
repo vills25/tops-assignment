0c500351fa648ac3e03aef1ab3803bb2da720b8c
vills25 <vishalsohaliya25@gmail.com>
2023-07-05 20:45:31 +0530

    Module 4.1 completed

---
+++ Module 4.1/hello_world.cpp
@@ -0,0 +1,14 @@
+// Q_1 WAP to print " HELLO WORLD " using C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    char a;
+
+    cout << "HELLO WORLD" << a << endl;
+
+    return 0;
+}
