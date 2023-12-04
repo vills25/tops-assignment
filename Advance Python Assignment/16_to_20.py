66c5007e31c29adb10dd7cea579658a6af5412c3
vills25 <132877214+vills25@users.noreply.github.com>
2023-12-04 12:41:38 +0530

    question no. 19 updated

--- Advance Python Assignment/16_to_20.py
+++ Advance Python Assignment/16_to_20.py
@@ -22,15 +22,18 @@ print("1" == 1)
 # 19. How Do You Handle Exceptions With Try/Except/Finally In Python? Explain with coding snippets.
 
 """
-print("start")
 try:
-    a = int(input("Enter something: "))
-    print(10/a)
-except ZeroDivisionError:
-    print("You can not divide by zero")
+    a=int(input("Enter Numvber1: "))
+    b=int(input("Enter Numvber2: "))
+    print(a/b)
+except ValueError as v:
+    print("Integers allowed",v)
+except ZeroDivisionError as z:
+    print("Not allowed to divide by zero",z)
+except:
+    print("Unknown Error")
 finally:
-    print("I will print any-way")
-print("end")
+    print("I am part of finally and I will execute always.")
 """
 ##################################################################################################################################################
 
