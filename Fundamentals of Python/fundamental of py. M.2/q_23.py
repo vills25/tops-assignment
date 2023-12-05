47c96241b4a2391ec81de87f0317b7a7fffaade3
vills25 <132877214+vills25@users.noreply.github.com>
2023-12-05 23:38:02 +0530

    Update q_23.py

--- Fundamentals of Python/fundamental of py. M.2/q_23.py
+++ Fundamentals of Python/fundamental of py. M.2/q_23.py
@@ -1,11 +1,9 @@
 # Write a Python function to insert a string in the middle of a string.
 
-def insert_middle(main_string, insert_string):
-    middle_index = len(main_string) // 2
-    result = main_string[:middle_index] + insert_string + " " + main_string[middle_index:]
-    return result
+original_string = input("Enter a string: ")
+string_to_insert = input("Enter a string to insert: ")
 
-original_string =(input( "Enter a string: "))
-string_to_insert = (input("Enter a string which you want to insert in previous string: "))
-result = insert_middle(original_string, string_to_insert)
+middle_index = len(original_string) // 2
+result = original_string[:middle_index] + string_to_insert + " " + original_string[middle_index:]
+
 print(result)
