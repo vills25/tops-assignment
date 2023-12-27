fe987c13cde95d34decbc202cd8c98f60b07cc10
vills25 <132877214+vills25@users.noreply.github.com>
2023-12-27 15:41:58 +0530

    Done

--- Assessment Advance Python/myvenv/Scripts/activate.bat
+++ Assessment Advance Python/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asus\OneDrive\Documents\test\myvenv
+set VIRTUAL_ENV=C:\Users\Asus\OneDrive\Desktop\Tops Assignment\tops-assignment\Assessment Advance Python\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
