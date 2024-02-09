55c58f3a44a9e77f67956546f2dc3c7201a16393
vills25 <132877214+vills25@users.noreply.github.com>
2024-02-09 11:30:26 +0530

    ##

--- Assignment python db & framework/make_django_application.py
+++
@@ -1,2 +0,0 @@
-# Make Django application to demonstrate following things o There will be 2 modules(Admin,Product manager) o Admin can add product name 
-#(ex.Product id and product name) ex. (1, Samsung), (2, Apple)...etc. Data should store in 
