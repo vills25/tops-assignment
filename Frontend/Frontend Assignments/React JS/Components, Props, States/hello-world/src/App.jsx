953f664f80becd50f30f55193e79806462a5d25b
Vishal Sohaliya <vishalsohaliya25@gmail.com>
2024-10-06 12:36:34 +0530

    New Assignments Added

---
+++ Frontend/Frontend Assignments/React JS/Components, Props, States/hello-world/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />       
+      </div>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
